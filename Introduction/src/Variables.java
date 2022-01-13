a4bfc5cca931537fa8c388c1bac73dd346dd2d46
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-13 13:58:17 -0300

    Getting to know variables

---
+++ Introduction/src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declarate a variable
+        int speed;
+        //To assing value
+        speed = 10;
+        System.out.println(speed);
+        int salary = 1000;
+        String employeeName = "Sr-Lechuga";
+        System.out.println(employeeName);
+    }
+}
