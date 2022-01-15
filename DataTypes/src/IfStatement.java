e50e4d4097c9ae867ac23d1448e1da740747094d
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-15 19:24:04 -0300

    If statement, with else

--- DataTypes/src/IfStatement.java
+++ DataTypes/src/IfStatement.java
@@ -1,12 +1,20 @@
 public class IfStatement {
     public static void main(String[] args) {
-        boolean isBluetoothEnabled = true;
+        boolean isBluetoothEnabled = false;
         int fileSended = 0;
 
         if (isBluetoothEnabled){
             //Send file
             fileSended++;
             System.out.println("Achive sended.");
+            int i = 0;
+            i++;
+        }else {
+            fileSended--;
+            System.out.println("Please turn on your Bluethoot, to start transference");
         }
+        System.out.println(isBluetoothEnabled);
+        System.out.println(fileSended);
+        //System.out.println(i); i is out of scope
     }
 }
