36ddb22d1bdbf9a81411b6d818fde7e01e9dfe45
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-16 17:27:47 -0300

    Arrays

---
+++ DataTypes/src/Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String [] androidVersions = new String[22];
+        String days[] = new String[7];
+
+        String [][] cities = new String[4][2]; //4 * 2 = 8
+        /*  ---------------------------
+           |   Country    |   City     |
+           ----------------------------
+           | Mexico       | CDMX       |
+           | Mexico       | Guadalajara|
+           | Uruguay      | Montevideo |
+           | Uruguay      | Canelones  |
+           ----------------------------
+         */
+
+        int [][][] numbers = new int [2][2][2]; // 2*2*2 = 8 elements
+        int [][][][] numbers_4th =  new int[2][2][2][2]; //2*2*2*2 = 16 elements
+    }
+}
