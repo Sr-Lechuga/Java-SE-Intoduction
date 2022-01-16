6ab2bd09bd56a8d8a66cbbff0fab6063352c88a4
Sr.Lechuga <jonattanlima@gmail.com>
2022-01-16 16:50:53 -0300

    Documentation with JAvadoc

--- DataTypes/src/Functions.java
+++ DataTypes/src/Functions.java
@@ -13,6 +13,11 @@ public class Functions {
         System.out.println("Pesos to dollars: " + convertToDolar(200,"MXN"));
     }
 
+    /**
+     * It returns the area of a circle
+     * @param r radio of circle
+     * @return A double with the value of area of the circle with r radius
+     */
     public static double circleArea(double r){
         return Math.PI * Math.pow(r,2);
     }
@@ -25,6 +30,12 @@ public class Functions {
         return 4/3 * Math.PI * Math.pow(r,2);
     }
 
+    /**
+     * Description: Function to convert to a value of dollars specificating a currency
+     * @param quantity Amount of money
+     * @param currency Type of currency: just MXN or COP
+     * @return quantity Returns the dollar amount
+     */
     public static double convertToDolar(double quantity, String currency){
         //MXN COP
         switch (currency){
